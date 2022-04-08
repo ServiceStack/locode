4c63f284e04ac8de167d46248441080507985006
Darren Reid <darren@reidmail.org>
2022-04-08 16:58:16 +1000

    Revert tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,9 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   content: [
     "./www/.vitepress/**/*.{vue,js,ts,md}",
     "./www/docs/*.md"
   ],
-  theme: {
-    extend: {
-      colors: {
-        sky: colors.sky,
-        teal: colors.teal,
-        rose: colors.rose,
-      },
-    },
-  },
+  theme: {},
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
