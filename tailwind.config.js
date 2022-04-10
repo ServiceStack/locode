51e72c9bfe0cb84557ffd43dc41338df3a2e108c
Demis Bellot <demis.bellot@gmail.com>
2022-04-10 17:52:10 +0800

    Add typography + docs config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,43 @@ module.exports = {
     "./www/.vitepress/**/*.{vue,js,ts,md}",
     "./www/docs/*.md"
   ],
-  theme: {},
+  theme: {
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: '""'
+            },
+            'code::after': {
+              content: '""'
+            },
+            maxWidth: 'unset',
+
+            a: {
+              color: theme(`colors.blue.600`),
+              textDecoration: `none`,
+              "&:hover": {
+                textDecoration: `underline`,
+              },
+            },
+
+            th: {
+              padding:'0.6em 1em'
+            },
+            td: {
+              padding:'0.6em 1em'
+            },
+
+          },
+        },
+      }),
+    }
+  },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
+    require('@tailwindcss/typography')({
+      className: 'content'
+    }),
   ],
 }
---
+++ www/public/custom.js
@@ -0,0 +1,25 @@
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-7722718-13', 'auto');
+ga('send', 'pageview');
+
+function enc(o) {
+    return o == null ? null : typeof o == 'string'
+        ? o.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/'/g, '&#39;').replace(/"/g, '&#34;')
+        : `${o}`
+}
+
+function copy(e) {
+    e.classList.add('copying')
+    let $el = document.createElement("textarea")
+    let text = (e.querySelector('code') || e.querySelector('p')).innerHTML
+    $el.innerHTML = text
+    document.body.appendChild($el)
+    $el.select()
+    document.execCommand("copy")
+    document.body.removeChild($el)
+    setTimeout(() => e.classList.remove('copying'), 3000)
+}
